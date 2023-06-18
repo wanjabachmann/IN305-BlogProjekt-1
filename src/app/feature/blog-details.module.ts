f7a5b27d9e49d651b2dc6d0bb8ea04d04a9dfd3f
wanjabachmann <wanja.bachmann@hotmail.com>
2023-06-18 19:17:24 +0200

    style: :lipstick: Makes the mat-card clickable

--- src/app/feature/blog-details.module.ts
+++ src/app/feature/blog-details.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogDetailsComponent } from './blog-details/blog-details.component';
-import { HomeComponent } from '../shared/home/home.component';
-import { HomeModule } from '../shared/home/home.module';
 
 const routes: Routes = [
   {
