c7a0cc23dd2f2136c2354bb560d5c3da7f9e44fa
wanjabachmann <wanja.bachmann@hotmail.com>
2023-06-25 14:58:49 +0200

    feat: :sparkles: Adds a text output to test the implemented state service on the overview page

--- src/app/feature/blog-overview/blog-overview.component.ts
+++ src/app/feature/blog-overview/blog-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BlogDataService, BlogResponse } from 'src/app/core/blog-data.service';
+import { StateService, Person } from 'src/app/core/state.service';
 
 @Component({
   selector: 'app-blog-overview',
@@ -9,9 +10,18 @@ import { BlogDataService, BlogResponse } from 'src/app/core/blog-data.service';
 })
 export class BlogOverviewComponent {
   blogs$: Observable<BlogResponse>;
+  currentState!: Person;
 
-  constructor(private blogService: BlogDataService) {
+  constructor(
+    private blogService: BlogDataService,
+    private stateService: StateService
+  ) {
     // saves the observable
     this.blogs$ = this.blogService.getBlogPosts();
+
+    // Update the current state if the observable emits a new value
+    this.stateService.state$.subscribe((state) => {
+      this.currentState = state;
+    });
   }
 }
